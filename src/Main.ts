9a686cb036697f6936766bf3dcedf5de6dbff944
Oliemanq <oliemanq456@gmail.com>
2026-02-01 00:35:30 -0600

    ooops api keys lol

--- src/Main.ts
+++ src/Main.ts
@@ -2,8 +2,8 @@ import { waitForEvenAppBridge } from '@evenrealities/even_hub_sdk';
 import SpotifyPresenter from './presenter/spotifyPresenter';
 
 console.log("Setting keys");
-const clientID = '29a961338df9480db3c1b50e10df184f'
-const clientSecret = 'af3da2d50cf44657842880cb5997f762'
+const clientID = ''
+const clientSecret = ''
 
 const spotifyPresenter = new SpotifyPresenter();
 
